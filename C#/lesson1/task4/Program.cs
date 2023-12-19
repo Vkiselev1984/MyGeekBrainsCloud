373ceadcdfd9fe89d0469feb51cb59da0861002a
Vladimir Kiselev <kiselev@gnivc.ru>
2023-12-19 22:32:06 +0300

    task4

---
+++ C#/lesson1/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 2;
+int b = 6;
+int c = 7;
+int d = 8;
+int e = 9;
+
+int max = a;
+
+if (b > max){
+    max = b;
+}
+
+if (c > max){
+    max = c;
+}
+
+if (d > max){
+    max = d;
+}
+
+if (e > max){
+    max = e;
+}
+
+Console.WriteLine(max);
