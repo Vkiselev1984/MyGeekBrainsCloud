579f034c78981cdb12dd82f1572d1b9723f162a2
Vladimir Kiselev <kiselev@gnivc.ru>
2023-12-24 22:01:06 +0300

    add lesson2

---
+++ C#/lesson2/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    //Проверка кратности числа 7 и 2
+static void Main()
+{
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0)
+{
+Console.WriteLine("да");
+}
+else
+{
+Console.WriteLine("нет");
+}
+}
+}
