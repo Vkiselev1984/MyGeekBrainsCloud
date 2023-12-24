bebc709b05ae0a38fb7bc1ba360b1fb670fa5f6c
Vladimir Kiselev <kiselev@gnivc.ru>
2023-12-24 22:25:32 +0300

    add task5

---
+++ C#/lesson2/task5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+public class Answer {
+    static void Main(string[] args)
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number >= 100 && number <= 999)
+    {
+        int firstDigit = number / 100;
+        int thirdDigit = number % 10;
+        int result = firstDigit * 10 + thirdDigit;
+        Console.WriteLine($"number = {number} => {result}");
+    }
+    else
+    {
+        Console.WriteLine("Число не трехзначное");
+    }
+}
+
+}
