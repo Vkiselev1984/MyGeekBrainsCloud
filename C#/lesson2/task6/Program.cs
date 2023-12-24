2bc7ad49875cfb935cf95acbc57e8d03bd4a9d43
Vladimir Kiselev <kiselev@gnivc.ru>
2023-12-24 23:16:53 +0300

    add task6 and task7

---
+++ C#/lesson2/task6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Answer {
+    static void Main(string[] args)
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number >= 100 && number <= 999)
+{
+    int secondDigit = number % 100 / 10;
+    int thirdDigit = number % 10;
+    int result = (int)Math.Pow(secondDigit, thirdDigit);
+    Console.WriteLine($"{number}: {secondDigit}^{thirdDigit} = {result}");
+    } 
+    else 
+    {
+    Console.WriteLine("Число не трехзначное");
+    }
+
+}
+}
---
+++ C#/lesson2/task7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// Напишите программу, которая выводит третью 
+// цифру слева заданного числа
+// 1234567 => 3
+// 7890 => 9
+// 123 => 3
+// 91 => Третьей цифры нет
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int copyNumber = number;
+if (number >= 100) // Число минимум трехзначное
+{
+    while (number >= 1000) // Делим, пока число имеет больше 3 зн
+    {
+        number /= 10; // number = number / 10
+    }
+    int thirdDigit = number % 10;
+    Console.WriteLine($"3 цифра от {copyNumber} = {thirdDigit}");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
