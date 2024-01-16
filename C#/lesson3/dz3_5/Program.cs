8fa78962564fb501a7f07c540264353eaf3e46c5
Vladimir Kiselev <kiselev@gnivc.ru>
2024-01-16 17:45:42 +0300

    add  dz_lesson 3_6

---
+++ C#/lesson3/dz3_5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем размер массива
+        int arraySize = 10;
+
+        // Создаем генератор случайных чисел
+        Random random = new Random();
+
+        // Создаем массив и заполняем его случайными трехзначными числами
+        int[] array = new int[arraySize];
+        for (int i = 0; i < arraySize; i++)
+        {
+            array[i] = random.Next(100, 1000); // Генерируем трехзначное число
+        }
+
+        // Выводим сгенерированный массив
+        Console.WriteLine("Сгенерированный массив:");
+        PrintArray(array);
+
+        // Находим количество чётных чисел в массиве
+        int evenCount = CountEvenNumbers(array);
+
+        // Выводим результат на консоль
+        Console.WriteLine($"Количество чётных чисел в массиве: {evenCount}");
+    }
+
+    // Метод для вывода массива на консоль
+    static void PrintArray(int[] array)
+    {
+        foreach (int number in array)
+        {
+            Console.Write($"{number} ");
+        }
+        Console.WriteLine();
+    }
+
+    // Метод для подсчета количества чётных чисел в массиве
+    static int CountEvenNumbers(int[] array)
+    {
+        int count = 0;
+
+        foreach (int number in array)
+        {
+            if (number % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ C#/lesson3/dz3_6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Пример массива
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // Вывод исходного массива
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        // Переворачиваем массив
+        ReverseArray(array);
+
+        // Вывод перевернутого массива
+        Console.WriteLine("Перевернутый массив:");
+        PrintArray(array);
+    }
+
+    // Метод для переворота массива
+    static void ReverseArray(int[] array)
+    {
+        int length = array.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            // Обмениваем значения элементов от начала к середине массива
+            int temp = array[i];
+            array[i] = array[length - 1 - i];
+            array[length - 1 - i] = temp;
+        }
+    }
+
+    // Метод для вывода массива на консоль
+    static void PrintArray(int[] array)
+    {
+        foreach (int number in array)
+        {
+            Console.Write($"{number} ");
+        }
+        Console.WriteLine();
+    }
+}
